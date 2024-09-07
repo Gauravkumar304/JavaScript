bfe25c75a3e5b0dc46d37409ab0173b5507de77f
Gauravkumar304 <118957031+Gauravkumar304@users.noreply.github.com>
2024-09-07 11:52:48 +0000

    variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 148742
+let accountEmail = 'pushkarkumarsingh7499@gmail.com'
+var accountPassword = '12345'
+accoutnCity = "Bangalore"
+let accoutnState;
+//accountId=2  // not allowed
+
+accountEmail = "hsfbkh@.com"
+accountPassword= "736"
+accoutnCity="jaipur"
+
+console.log(accountId);
+/*
+prefer no to use var
+because of issue in block scope and funstional scope
+
+*/
+
+console.table([accountId, accountEmail,accountPassword,accoutnCity,accoutnState])
+
