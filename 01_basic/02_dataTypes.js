ede67e70da9f5bb96d20b89505415574621c727e
Gauravkumar304 <118957031+Gauravkumar304@users.noreply.github.com>
2024-09-13 15:04:46 +0000

    adding datatype

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS code as newer version
+
+// alert("hello")
+// alert(3 + 3)  // we are using nodejs , not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Gaurav")
+
+let name = "Gaurav"
+let age = 21
+let isLoggedIn = false
+let state;  //undefined
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value  
+//undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Gaurav");  //string
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
