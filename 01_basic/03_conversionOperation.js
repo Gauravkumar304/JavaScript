83e77baf3877c8c2d89b3f9de9340791016dadbf
Gauravkumar304 <118957031+Gauravkumar304@users.noreply.github.com>
2024-09-13 15:30:46 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+// let score = null
+// let score = true
+let score = "Gaurav"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true =>1; false =>0
+
+let isLoggedIn = "Gaurav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Gaurav" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
